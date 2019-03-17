426342a18ca401d61801701b472457670596116f
Henrik Panhans <henrikpanhans@icloud.com>
2019-03-17 10:41:49 +0100

    Initial Commit

---
+++ WalletTableDemoApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WalletTableDemoApp
+//
+//  Created by Henrik Panhans on 17.03.19.
+//  Copyright © 2019 Henrik Panhans. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
